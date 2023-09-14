6d31a00e1c040f953d210771de72e1d4a296abfe
wuchi <wuchi57@outlook.com>
2023-09-14 14:31:16 +0800

    five

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Koa = require('koa');
+const Koa = require('koa')
 
 const app = new Koa()
 
 // response
 app.use((ctx) => {
-  ctx.body = 'Hello Koa';
+  ctx.body = 'Hello Koa'
 })
 
-app.listen(3000);
+app.listen(3000)
